8885efd0f5bb1b266667a6f58f52abb6f2193f66
KirillGorinDataArt <chetanvse@gmail.com>
2025-08-13 20:53:13 -0300

    task

--- lists.go
+++ lists.go
@@ -3,12 +3,66 @@ package main
 import "fmt"
 
 func main() {
-	prices := []float64{10.99, 8.99}
-	fmt.Println(prices[0])
+	// 1
+	hobbies := [3]string{"Reading", "Gaming", "Walking"}
+	fmt.Println(hobbies)
 
-	updatedPrices := append(prices, 13.99)
-	fmt.Println(updatedPrices)
+	// 2
+	fmt.Println(hobbies[0])
+	fmt.Println(hobbies[1:])
+
+	// 3
+	firstSlice := hobbies[:2]
+	secondSlice := firstSlice[:2]
+	fmt.Println(firstSlice, secondSlice)
+
+	// 4
+	thirdSlice := firstSlice[1:3]
+	fmt.Println(thirdSlice)
+
+	// 5
+	goals := []string{"learn", "improve attention span"}
+
+	// 6
+	goals[1] = "different"
+	goals = append(goals, "new goal")
+	fmt.Println("goals", goals)
+
+	// 7
+	type Product struct {
+		title string
+		id    string
+		price float64
+	}
+
+	products := []Product{
+		{
+			title: "Phone",
+			id:    "1",
+			price: 999.99,
+		},
+		{
+			title: "Tablet",
+			id:    "2",
+			price: 699.99,
+		},
+	}
+	fmt.Println(products)
+	products = append(products, Product{
+		title: "Headphones",
+		id:    "3",
+		price: 399.99,
+	})
+	fmt.Println(products)
 }
+
+//func main() {
+//	prices := []float64{10.99, 8.99}
+//	fmt.Println(prices[0])
+//
+//	updatedPrices := append(prices, 13.99)
+//	fmt.Println(updatedPrices)
+//}
 
 //func main() {
 //	var productNames [4]string = [4]string{"A Book"}
