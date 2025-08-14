1fbdce1e58b7fc2eca5c7f6360dab4e2ba809988
KirillGorinDataArt <chetanvse@gmail.com>
2025-08-13 21:03:20 -0300

    maps in go

---
+++ maps.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	websites := map[string]string{
+		"Google":              "https://google.com",
+		"Yahoo":               "https://yahoo.com",
+		"Amazon Web Services": "https://aws.com",
+	}
+	fmt.Println(websites)
+}
