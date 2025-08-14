9a7d5a8455076c89aab083c4e3a97cf6d4240cbd
KirillGorinDataArt <chetanvse@gmail.com>
2025-08-13 21:19:55 -0300

    make'ing maps

--- main.go
+++ main.go
@@ -9,4 +9,10 @@ func main() {
 	userNames = append(userNames, "Chevy")
 	userNames = append(userNames, "Ram")
 	fmt.Println(userNames)
+
+	courseRatings := make(map[string]float64, 3)
+	courseRatings["Go"] = 4.5
+	courseRatings["Python"] = 4.7
+	courseRatings["Java"] = 4.9
+	fmt.Println(courseRatings)
 }
